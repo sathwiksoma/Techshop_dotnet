c85de3cc4d650710127c6fbde91a0355e05ebf5b
Soma Sathwik <61172852+sathwiksoma@users.noreply.github.com>
2024-02-05 15:45:50 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,39 +10,37 @@ namespace assignment
         static void Main(string[] args)
         {
             ICustomerService customerService = new CustomerService();
-            //customerService.CalculateTotalOrders(4);
-            //customerService.GetCustomerDetails();
-            //customerService.UpdateCustomerInfo(12,"eww@gmail.com");
+            customerService.CalculateTotalOrders(4);
+            customerService.GetCustomerDetails();
+            customerService.UpdateCustomerInfo(12,"eww@gmail.com");
 
             IProductService productService = new ProductService();
-            //productService.GetProductDetails(1);
-            //productService.UpdateProductInfo(2);
-            //productService.IsProductInStock(2);
+            productService.GetProductDetails(1);
+            productService.UpdateProductInfo(2);
+            productService.IsProductInStock(2);
 
             IOrderService orderService = new OrderService();
-            //orderService.CalculateTotalAmount(3);
-            //orderService.GetOrderDetails();
-            //orderService.UpdateOrderStatus(3,"shipped");
-            //orderService.CancelOrder();
+            orderService.CalculateTotalAmount(3);
+            orderService.GetOrderDetails();
+            orderService.UpdateOrderStatus(3,"shipped");
 
             IOrderDetailService orderDetailService = new OrderDetailService();
-            //orderDetailService.GetOrderDetailsById(2);
-            //orderDetailService.CalculateSubtotal(2);
-            //orderDetailService.UpdateQuantity(3, 45);
-            //orderDetailService.AddDiscount(8,19);
+            orderDetailService.GetOrderDetailsById(2);
+            orderDetailService.CalculateSubtotal(2);
+            orderDetailService.UpdateQuantity(3, 45);
+            orderDetailService.AddDiscount(8,19);
 
             IInventoryService inventoryService = new InventoryService();
-            //inventoryService.GetProduct(2);
-            //inventoryService.GetQuantityInStock(3);
-            //inventoryService.AddToInventory(3,20);
-            //inventoryService.RemoveFromInventory(3,20);
-            //inventoryService.UpdateStockQuantity(8,10);
-            //inventoryService.IsProductAvailable(4,2);
-            //inventoryService.GetInventoryValue(5);
-            //inventoryService.ListLowStockProducts(55);
-            //inventoryService.ListOutOfStockProducts();
-            //inventoryService.ListAllProducts();
-
+            inventoryService.GetProduct(2);
+            inventoryService.GetQuantityInStock(3);
+            inventoryService.AddToInventory(3,20);
+            inventoryService.RemoveFromInventory(3,20);
+            inventoryService.UpdateStockQuantity(8,10);
+            inventoryService.IsProductAvailable(4,2);
+            inventoryService.GetInventoryValue(5);
+            inventoryService.ListLowStockProducts(55);
+            inventoryService.ListOutOfStockProducts();
+            inventoryService.ListAllProducts();
         }
     }
 }
